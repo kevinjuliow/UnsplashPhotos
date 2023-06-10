e60e4ffbc9ec12a7e415b12acf5211f386269281
kevinjuliow <kevjuliow@gmail.com>
2023-06-10 23:03:09 +0700

    commit

--- src/Pages/Collections/Collection.jsx
+++ src/Pages/Collections/Collection.jsx
@@ -1,6 +1,10 @@
+import axios from 'axios'
 import React from 'react'
 
 const Collection = () => {
+  const fetchCollections = async () =>{
+    const response = await axios.get('https')
+  }
   return (
     <div>Collection</div>
   )
--- src/Pages/Random/Random.jsx
+++ src/Pages/Random/Random.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import HomeImages from '../Home/HomeImages'
+import Masonry ,{ResponsiveMasonry} from 'react-responsive-masonry'
+import './Random.css'
 
 const Random = () => {
+  const [randomPhotos , setRandomPhotos] = useState([])
+  const fetchRandom = async() =>{
+    try {
+      const response = await axios.get('https://api.unsplash.com/photos/random' , {
+        params : {
+          client_id : 'cbcE4BHkUayOSBaspLsLOTte_DGYVyanMXS5nXF2NfM' , 
+          count : 10
+        }
+      })
+      setRandomPhotos(response.data)
+    }catch(error) {
+      console.error('Error Fetching')
+    }
+    
+    
+  }
+
+  useEffect(()=>{
+  //  fetchRandom()
+  })
   return (
-    <div>Random</div>
+    <div className='random-page'>
+      asd
+        <>
+        <ResponsiveMasonry
+                columnsCountBreakPoints={{350: 1, 750: 2, 900: 3}}
+            >
+                <Masonry gutter='10px'> 
+                {randomPhotos.map((e)=>{
+                      return (
+                  <HomeImages data = {e} />
+                    )
+                        })}
+                </Masonry>
+            </ResponsiveMasonry>
+        
+        </>
+    </div>
   )
 }
 
