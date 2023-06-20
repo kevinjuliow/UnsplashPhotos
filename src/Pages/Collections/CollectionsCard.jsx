95744332e95511de60ae4eeb479722c214ea7947
kevinjuliow <kevjuliow@gmail.com>
2023-06-20 20:50:12 +0700

    commit

--- src/Pages/Collections/CollectionsCard.jsx
+++ src/Pages/Collections/CollectionsCard.jsx
@@ -5,11 +5,11 @@ const CollectionsCard = (props) => {
   const {cover_photo , preview_photos} = props.data
   return (
     <div className='collectionsCard'>
-      <div style={{display:'flex' , height :'80%' , width :'70%'}}>
-        <img src={preview_photos[0].urls.raw} alt="" style={{width:'100%', objectPosition:"center" , objectFit:'fill'}}/>
+      <div style={{display:'flex' , height :'80%' }}>
+        <img src={preview_photos[0].urls.small} alt="" style={{ objectPosition:"center" , objectFit:'fill'}}/>
         <div style={{display :'flex' , flexDirection : 'column' , height :'20%' , width :'30%'}}>
           
-        </div>
+        </div>  
       </div>
       {cover_photo.alt_description}
       </div>
