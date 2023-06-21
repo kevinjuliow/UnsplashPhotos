ec31ee7d23c42fb0650270726e77ecec908615dd
kevinjuliow <kevjuliow@gmail.com>
2023-06-21 18:12:08 +0700

    commit

--- src/Pages/Collections/CollectionsCard.jsx
+++ src/Pages/Collections/CollectionsCard.jsx
@@ -2,17 +2,26 @@ import React from 'react'
 import './Collections.css'
 
 const CollectionsCard = (props) => {
-  const {cover_photo , preview_photos} = props.data
+  const {cover_photo , preview_photos , tags} = props.data
   return (
     <div className='collectionsCard'>
-      <div style={{display:'flex' , height :'80%' }}>
-        <img src={preview_photos[0].urls.small} alt="" style={{ objectPosition:"center" , objectFit:'fill'}}/>
-        <div style={{display :'flex' , flexDirection : 'column' , height :'20%' , width :'30%'}}>
-          
+      <div style={{display:'flex' , height :'320px' , width :'280px'}}>
+        <div>
+        <img src={preview_photos[0].urls.regular} alt="" style={{ width :'280px', height :"320px",  objectPosition:"center" , objectFit:'cover'}}/>
+        </div>
+        <div style={{display :'flex' , flexDirection : 'column' , height :'320px' , width :'120px' , marginLeft :'10px' , rowGap :"20px" }}>
+        <img src={preview_photos[1].urls.thumb} alt="" style={{ width :'120px', height :"150px",  objectPosition:"center" , objectFit:'cover'}}/>
+        <img src={preview_photos[2].urls.thumb} alt="" style={{ width :'120px', height :"150px",  objectPosition:"center" , objectFit:'cover'}}/>          
         </div>  
       </div>
-      {cover_photo.alt_description}
+      <p className='collectionsTitle'>{cover_photo.alt_description}</p>
+      <div>
+      <p className='tags'>{tags[0].title}</p>
+      <p className='tags'>{tags[1].title}</p>
+      <p className='tags'>{tags[2].title}</p>
       </div>
+   
+      </div> 
   )
 }
 
